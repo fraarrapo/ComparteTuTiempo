9a648fb31a606bfb2c75ff1de70b432ab2f31ffc
fraarrapo <fraarrapo@alum.us.es>
2021-09-28 13:08:08 +0200

    feature-003 Configuración:
    Configuración para despliegue

--- ComparteTuTiempo/settings.py
+++ ComparteTuTiempo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'ComparteTuTiempoApp.Usuario'
+
+django_heroku.settings(locals())
